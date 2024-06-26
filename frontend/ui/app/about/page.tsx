aad55b3c5954eb4b5d532f8cff4988127cc23ba0
Dzenis Djuhic <dzenis.djuhic@edu.fh-joanneum.at>
2024-06-26 20:04:55 +0200

    added directions for different pages and navbar

---
+++ frontend/ui/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About Pawsitive</h1>
+        </div>
+    );
+}
---
+++ frontend/ui/app/shop/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ShopPage() {
+    return (
+        <main>
+            <h1>Shop</h1>
+            <p>Get your Pawsitive Collar</p>
+        </main>
+    );
+}
